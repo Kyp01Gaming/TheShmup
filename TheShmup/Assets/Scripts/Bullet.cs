eec9ad95cbe1586d03201c7873590be21af3af04
Kyp01Gaming <agm.l@hotmail.com>
2016-09-08 19:26:15 +1000

    Scripts folder containing scritps.
    The scripts are the Bullet Script and the Enemy Script

---
+++ TheShmup/Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    // Speed of the Bullet
+    public float speed = 20.0f;
+
+    // Reference to own transform
+    private Transform myTransform;
+        
+    void Awake()
+    {
+        // Gets reference to own transform
+        myTransform = gameObject.transform;
+    }
+
+    void Update()
+    {
+        // Moves the Bullet
+        myTransform.position += myTransform.forward * speed * Time.deltaTime;
+    }
+
+    // Destruction of object when an enemy bullet and player bullet collide
+    public void Hurt()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        // Checks for an object with either a player tag or an enemy tag while this isn't tagged enemy
+        if (col.gameObject.tag == "Player" || (gameObject.tag != "Enemy" && col.gameObject.tag == "Enemy"))
+            col.gameObject.SendMessage("Hurt");
+        if (!(gameObject.tag == "Enemy" && col.gameObject.tag == "Enemy"))
+            Destroy(gameObject);
+    }
+}
---
+++ TheShmup/Assets/Scripts/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    // Reference to bullet
+    public GameObject bullet;
+    
+    // Private variables
+    private float shotTimer = 0;
+    private float speed;
+    private float nextShot = 1;
+
+    // Shooting time
+    public float minShotTime = 1.5f;
+    public float maxShotTime = 3.5f;
+
+    // Reference to own Transform
+    private Transform myTransform;
+
+	// Use this for initialization
+	void Awake () {
+        // Sets reference to transform
+        myTransform = gameObject.transform;
+        // Set speed to a random range to add diversity
+        speed = Random.Range(2, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Shooting Logic
+        shotTimer += Time.deltaTime;
+        if (shotTimer > nextShot)
+        {
+            GameObject reff = Instantiate(bullet, myTransform.position + (myTransform.forward * 1.25f), myTransform.rotation) as GameObject;
+            reff.tag = "Enemy";
+            shotTimer = 0;
+            nextShot = Random.Range(1.5f, 3.5f);
+        }
+
+        // Movement
+        myTransform.position += myTransform.forward * speed * Time.deltaTime;
+    }
+
+    public void Hurt()
+    {
+        GameManager.refer.IncreaseScore(10);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.SendMessage("Hurt");
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.tag != "Enemy")
+            Destroy(gameObject);
+    }
+}
